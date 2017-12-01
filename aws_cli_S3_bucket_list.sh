28781bc69b0484fb955270fe563ed9d86942d74b
Joey R Brown <joeyrbrown@icloud.com>
2017-12-01 02:23:29 -0600

    Added command line interactivity.

--- aws_cli_S3_bucket_list.sh
+++ aws_cli_S3_bucket_list.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # Info - http://parthicloud.com/how-to-access-s3-bucket-from-application-on-amazon-ec2-without-access-credentials/
 # Info - http://docs.aws.amazon.com/cli/latest/userguide/using-s3-commands.html
-# AWS CLI command for S3 to list contents in bucket ec2tempbucket
-aws s3 ls s3://ec2tempbucket
+# https://www.shellscript.sh/variables1.html
+echo What is the S3 Bucket Name?
+read S3_BUCKET_NAME
+list=$(aws s3 ls s3://$S3_BUCKET_NAME)
+echo -e "The contents of your S3 bucket $S3_BUCKET_NAME are: \n$list"
+
+
